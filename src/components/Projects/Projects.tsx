fa49254eeacd9c18b80753d13f4976afd5be919d
Nathan Newyen <nathan@nathannewyen.com>
2024-04-26 16:43:42 -0400

    add project component

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -4,6 +4,18 @@ const Projects = () => {
     return (
         <div>
             <h1>Projects</h1>
+
+            <h2>Software Engineer, Trade Aries</h2>
+
+            <div>
+                Developed a Vue component/package for the TradingView trading terminal, integrating it into existing or new Vue pages
+            </div>
+            <div>
+                Utilized the TradingView charting library and adhered to their documentation for seamless integration
+            </div>
+            <div>
+                Ensured real-time communication with the server using TradingView's proprietary API
+            </div>
         </div>
     );
 }
