1cddd9e0d2576c14943d25f71577389390934a93
Nathan Newyen <nathan@nathannewyen.com>
2024-05-05 17:54:44 -0400

    add thumbnail image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import type { Metadata } from "next";
+import Head from 'next/head';
 import StyledComponentsRegistry from '../lib/registry'
 import GlobalStyle from '@/styles/GlobalStyle';
 import GradientCircle from "@components/GradientCircle/GradientCircle";
@@ -7,6 +8,19 @@ import GradientCircle from "@components/GradientCircle/GradientCircle";
 export const metadata: Metadata = {
   title: "Nhan Nguyen",
   description: "This is a Next.js project developed by Nhan Nguyen. It showcases proficiency in JavaScript, TypeScript, React, and npm.",
+    openGraph: {
+        type: 'website',
+        locale: 'en_US',
+        url: 'https://nathan-v3.vercel.app/',
+        images: [
+            {
+                url: '/images/thumbnail.png',
+                width: 800,
+                height: 600,
+                alt: 'Nhan Nguyen',
+            },
+        ],
+    },
 };
 
 export default function RootLayout({
